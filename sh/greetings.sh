314aa5b2079bbca85c20da55e394e5fe40170170
Ивлев Вадим <ivlev@rg.ru>
2019-11-20 17:44:18 +0300

    fix: deployment

--- sh/greetings.sh
+++ sh/greetings.sh
@@ -2,7 +2,7 @@
 
 echo "Started."
 echo "Original image"
-echo "http://localhost/2019/09/25/30614/42.jpg"
+echo "http://localhost:5080/2019/09/25/30614/42.jpg"
 echo "Resized image"
-echo "http://localhost/320x240/2019/09/25/30614/42.jpg"
+echo "http://localhost:5080/320x240/2019/09/25/30614/42.jpg"
 echo "Next requests will return the cached image ..."
