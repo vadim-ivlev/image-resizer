b697710ab6c8e561ecb99c0d1cf6870976084d49
Ивлев Вадим <ivlev@rg.ru>
2019-11-22 16:49:00 +0300

    .

--- sh/greetings.sh
+++ sh/greetings.sh
@@ -7,6 +7,6 @@ echo "Original image"
 echo "   http://localhost:5080/2019/09/25/30614/42.jpg"
 echo
 echo "Resized image"
-echo "   http://localhost:5080/320x240/2019/09/25/30614/42.jpg"
+echo "   http://localhost:5080/resize320x150/2019/09/25/30614/42.jpg"
 echo
 echo "Following requests will return the cached image"
